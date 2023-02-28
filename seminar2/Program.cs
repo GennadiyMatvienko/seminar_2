a52e61ff58a55ac67c7d04cdb7e8ac7aee90427a
Геннадий Матвиенко <gennady39@hotmail.com>
2023-03-01 01:00:20 +0300

    seminar commit

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача 4 напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+Console.Write("Введите первое число");
+number_1 = Console.int.Parse(Console.Readline());
+
+Console.Write("Введите второе число");
+number_2 = Console.int.Parse(Console.Readline());
+
+Console.Write("Введите третье число");
+number_3 = Console.int.Parse(Console.Readline());
+int max = number_1;
+
+if(number_2 > max){
+	max = number_2;
+}
+if(number_3 > max){
+	max = number_3;
+}
+Console.Write($"Наибольшое число: {max}");
